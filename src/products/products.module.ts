9d0b9a9c2468e9059c1b628e619b1443082f5538
tabarino <tabarino@outlook.com>
2020-09-18 20:38:34 +0100

    Creating Reducer

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,6 +3,8 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './store';
 
 // components
 import * as fromComponents from './components';
@@ -35,9 +37,12 @@ export const ROUTES: Routes = [
         ReactiveFormsModule,
         HttpClientModule,
         RouterModule.forChild(ROUTES),
+        StoreModule.forFeature('products', reducers)
     ],
     providers: [...fromServices.services],
     declarations: [...fromContainers.containers, ...fromComponents.components],
     exports: [...fromContainers.containers, ...fromComponents.components],
 })
-export class ProductsModule { }
+
+export class ProductsModule {
+}
--- src/products/store/actions/pizzas.action.ts
+++ src/products/store/actions/pizzas.action.ts
@@ -1,25 +1,23 @@
 import { Action } from '@ngrx/store';
-
 import { Pizza } from '../../models/pizza.model';
 
-// All of this was created in this lesson (commit)
 // load pizzas
 export const LOAD_PIZZAS = '[Products] Load Pizzas';
 export const LOAD_PIZZAS_FAIL = '[Products] Load Pizzas Fail';
 export const LOAD_PIZZAS_SUCCESS = '[Products] Load Pizzas Success';
 
 export class LoadPizzas implements Action {
-  readonly type = LOAD_PIZZAS;
+    readonly type = LOAD_PIZZAS;
 }
 
 export class LoadPizzasFail implements Action {
-  readonly type = LOAD_PIZZAS_FAIL;
-  constructor(public payload: any) {}
+    readonly type = LOAD_PIZZAS_FAIL;
+    constructor(public payload: any) { }
 }
 
 export class LoadPizzasSuccess implements Action {
-  readonly type = LOAD_PIZZAS_SUCCESS;
-  constructor(public payload: Pizza[]) {}
+    readonly type = LOAD_PIZZAS_SUCCESS;
+    constructor(public payload: Pizza[]) { }
 }
 
 // action types
